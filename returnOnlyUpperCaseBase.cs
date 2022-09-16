dcf481988a45915a30001bd897b71ceac7dcb237
Mukhlisa17 <112000599+Mukhlisa17@users.noreply.github.com>
2022-09-16 09:58:13 +0500

    Add files via upload

---
+++ returnOnlyUpperCaseBase.cs
@@ -0,0 +1,19 @@
+﻿namespace Homwework
+{
+    public class returnOnlyUpperCaseBase
+    {
+        public static void Main()
+        {
+            string word = "DDD example CQRS Event Sourcing";
+
+            var returnUppercaseOnly = word.Split(' ').Where(x => string.Equals(x, x.ToUpper()));
+
+            foreach (var u in returnUppercaseOnly)
+            {
+                Console.Write($"{u}, ");
+            }
+
+
+        }
+    }
+}
